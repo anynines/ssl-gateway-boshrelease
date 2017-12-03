9f61c0c13640cc7f8c225a38621d68dc74780922
gdenn <gdennis91@googlemail.com>
2017-12-03 22:28:32 +0100

    Remove helper from spec_helper

--- integration-tests/spec/spec_helper.rb
+++ integration-tests/spec/spec_helper.rb
@@ -5,12 +5,16 @@ Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.before(:suite) do
-    BoshHelper::create_dev_release
-    BoshHelper::upload_last_dev_release
+    Dir.chdir(File.join(__dir__, "../../../")) do
+      `bosh create-release --force`
+      ENV["RELEASE"] = `ls -1t dev_releases/ssl-gateway | sed -n 2p`
+      `bosh upload-release dev_releases/ssl-gateway/#{ENV["RELEASE"]}`
+      `bosh task`
+    end
   end
 
   config.after(:suite) do
-    BoshHelper::cleanup_all
+    `bosh clean-up --all`
   end
 
   config.color = true
