1408a119bc234677439dce582964470a3e54cb37
gdenn <gdennis91@googlemail.com>
2017-12-14 23:31:09 +0100

    Upload s3 blobs

--- ci/task.rb
+++ ci/task.rb
@@ -11,27 +11,27 @@ RUBYGEMS_DOWNLOAD_URL = "https://github.com/rubygems/rubygems/archive/v__version
 BUNDLER_DOWNLOAD_URL = "https://github.com/bundler/bundler/archive/v__version__.tar.gz"
 
 def download_blob(name, template, version)
-    blob_dir = "/tmp/#{name}/"
-    `rm -rf #{blob_dir}`
-    download_url = template.gsub("__version__", version)
-    `wget #{download_url} -P #{blob_dir}`
-    File.basename(Dir["/tmp/#{name}/*"][0])
+  blob_dir = "/tmp/#{name}/"
+  `rm -rf #{blob_dir}`
+  download_url = template.gsub("__version__", version)
+  `wget #{download_url} -P #{blob_dir}`
+  File.basename(Dir["/tmp/#{name}/*"][0])
 end
 
 def remove_blob(name)
-    blob_name = name.split(".")[0]
-    `bosh remove-blob -n /#{blob_name}/#{name}`
+  blob_name = name.split(".")[0]
+  `bosh remove-blob -n /#{blob_name}/#{name}`
 end
 
 def add_blob(name, blob)
-    blob_name = name.split(".")[0]
-    download_name = File.basename(Dir["/tmp/*"][0])
-    `bosh add-blob -n /tmp/#{blob_name}/#{blob} /#{blob_name}/#{name}`
+  blob_name = name.split(".")[0]
+  download_name = File.basename(Dir["/tmp/*"][0])
+  `bosh add-blob -n /tmp/#{blob_name}/#{blob} /#{blob_name}/#{name}`
 end
 
 def upload_blobs
-    `envsubst < config/private.yml.example > config/private.yml`
-    `bosh upload-blobs`
+  `envsubst < config/private.yml.example > config/private.yml`
+  `bosh upload-blobs`
 end
 
 # update nginx
@@ -73,3 +73,5 @@ add_blob("rubygems.tar.gz", blob)
 remove_blob("bunlder.tar.gz")
 blob =  download_blob("bundler", BUNDLER_DOWNLOAD_URL, ENV["BUNDLER_VERSION"])
 add_blob("bundler.tar.gz", blob)
+
+upload_blobs
