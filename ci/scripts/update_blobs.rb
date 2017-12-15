d06f11e671debd994b0b75acde032855c231e9e2
gdenn <gdennis91@googlemail.com>
2017-12-15 16:50:37 +0100

    Fix packaging

--- ci/scripts/update_blobs.rb
+++ ci/scripts/update_blobs.rb
@@ -19,15 +19,13 @@ def download_blob(name, template, version)
   File.basename(Dir["/tmp/#{name}/*"][0])
 end
 
-def remove_blob(name)
-  blob_name = name.split(".")[0]
-  `bosh remove-blob -n /#{blob_name}/#{name}`
+def remove_blob(blob_dir, name)
+  `bosh remove-blob -n /#{blob_dir}/#{name}`
 end
 
-def add_blob(name, blob)
+def add_blob(blob_dir, name, blob)
   blob_name = name.split(".")[0]
-  download_name = File.basename(Dir["/tmp/*"][0])
-  `bosh add-blob -n /tmp/#{blob_name}/#{blob} /#{blob_name}/#{name}`
+  `bosh add-blob -n /tmp/#{blob_name}/#{blob} /#{blob_dir}/#{name}`
 end
 
 def upload_blobs
@@ -36,48 +34,48 @@ def upload_blobs
 end
 
 # update nginx
-remove_blob("nginx.tar.gz")
+remove_blob("nginx", "nginx.tar.gz")
 blob =  download_blob("nginx", NGINX_DOWNLOAD_URL, ENV["NGINX_VERSION"])
-add_blob("nginx.tar.gz", blob)
-
-# update libyaml
-remove_blob("libyaml.tar.gz")
-blob =  download_blob("libyaml", LIBYAML_DOWNLOAD_URL, ENV["LIBYAML_VERSION"])
-add_blob("libyaml.tar.gz", blob)
-
-# update libyaml
-remove_blob("ruby.tar.gz")
-blob =  download_blob("ruby", RUBY_DOWNLOAD_URL, ENV["RUBY_VERSION"])
-add_blob("ruby.tar.gz", blob)
-
-# update zlib
-remove_blob("zlib.tar.gz")
-blob =  download_blob("zlib", ZLIB_DOWNLOAD_URL, ENV["ZLIB_VERSION"])
-add_blob("zlib.tar.gz", blob)
+add_blob("nginx", "nginx.tar.gz", blob)
 
 # update pcre
-remove_blob("pcre.tar.gz")
+remove_blob("nginx", "pcre.tar.gz")
 blob =  download_blob("pcre", PCRE_DOWNLOAD_URL, ENV["PCRE_VERSION"])
-add_blob("pcre.tar.gz", blob)
+add_blob("nginx", "pcre.tar.gz", blob)
+
+# update zlib
+remove_blob("zlib", "zlib.tar.gz")
+blob =  download_blob("zlib", ZLIB_DOWNLOAD_URL, ENV["ZLIB_VERSION"])
+add_blob("zlib", "zlib.tar.gz", blob)
 
 # update postgres
-remove_blob("postgres.tar.gz")
+remove_blob("postgres", "postgres.tar.gz")
 blob =  download_blob("postgres", POSTGRES_DOWNLOAD_URL, ENV["POSTGRES_VERSION"])
-add_blob("postgres.tar.gz", blob)
+add_blob("postgres", "postgres.tar.gz", blob)
 
-# update rubygems
-remove_blob("rubygems.tar.gz")
-blob =  download_blob("rubygems", RUBYGEMS_DOWNLOAD_URL, ENV["RUBYGEMS_VERSION"])
-add_blob("rubygems.tar.gz", blob)
-
-# update bundler
-remove_blob("bunlder.tar.gz")
-blob =  download_blob("bundler", BUNDLER_DOWNLOAD_URL, ENV["BUNDLER_VERSION"])
-add_blob("bundler.tar.gz", blob)
+# update libyaml
+remove_blob("libyaml", "libyaml.tar.gz")
+blob =  download_blob("libyaml", LIBYAML_DOWNLOAD_URL, ENV["LIBYAML_VERSION"])
+add_blob("libyaml", "libyaml.tar.gz", blob)
 
 # update autoconf
-remove_blob("autoconf.tar.gz")
+remove_blob("libyaml", "autoconf.tar.gz")
 blob =  download_blob("autoconf", AUTOCONF_DOWNLOAD_URL, ENV["AUTOCONF_VERSION"])
-add_blob("autoconf.tar.gz", blob)
+add_blob("libyaml", "autoconf.tar.gz", blob)
+
+# update libyaml
+remove_blob("ruby", "ruby.tar.gz")
+blob =  download_blob("ruby", RUBY_DOWNLOAD_URL, ENV["RUBY_VERSION"])
+add_blob("ruby", "ruby.tar.gz", blob)
+
+# update rubygems
+remove_blob("ruby", "rubygems.tar.gz")
+blob =  download_blob("rubygems", RUBYGEMS_DOWNLOAD_URL, ENV["RUBYGEMS_VERSION"])
+add_blob("ruby", "rubygems.tar.gz", blob)
+
+# update bundler
+remove_blob("ruby", "bunlder.tar.gz")
+blob =  download_blob("bundler", BUNDLER_DOWNLOAD_URL, ENV["BUNDLER_VERSION"])
+add_blob("ruby", "bundler.tar.gz", blob)
 
 upload_blobs
