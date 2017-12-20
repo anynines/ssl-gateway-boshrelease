e7168ebd46f98de722fab80fe9eeec1ad308808a
deployments <sschmidt@anynines.com>
2017-12-20 15:29:48 +0000

    Add run script for acceptance suite

--- acceptance-tests/run-testsuite.sh
+++ acceptance-tests/run-testsuite.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 export DEFAULT_APP_DOMAIN=de.a9sapp.eu
-export RANDOM_DOMAIN=checker.misterX.com
-export UNREACHABLE_BLACKLIST_DOMAIN=checker.ssltest3.com
-export REACHABLE_BLACKLIST_DOMAIN=checker.ssltest4.com
-export UNREACHABLE_SSL_BLACKLIST_DOMAIN=checker.ssltest2.com
-export REACHABLE_SSL_BLACKLIST_DOMAIN=checker.ssltest.com
+export RANDOM_DOMAIN=misterX.com
+export UNREACHABLE_BLACKLIST_DOMAIN=ssltest3.com
+export REACHABLE_BLACKLIST_DOMAIN=ssltest4.com
+export UNREACHABLE_SSL_BLACKLIST_DOMAIN=ssltest2.com
+export REACHABLE_SSL_BLACKLIST_DOMAIN=ssltest.com
 export OPS_FILE=/home/vcap/bosh/anynines-PaaS-deployment/ssl-gateway/ops/vSphere-networks.yml
 export IAAS_CONFIG=/home/vcap/bosh/anynines-PaaS-deployment/iaas-config/a9s-staging-vsphere.yml
 export EXTERNAL_SECRETS=/home/vcap/bosh/anynines-deployment/secrets/external-secrets.yml
+export LOCALHOST_IP=172.27.1.5
 
 cf auth $CF_USER $CF_PASSWORD
 
@@ -20,11 +21,11 @@ cf create-space test
 cf target -s test
 
 cf create-domain ssl-gateway-acceptance de.a9sapp.eu
-cf create-domain ssl-gateway-acceptance checker.misterX.com
-cf create-domain ssl-gateway-acceptance checker.ssltest3.com
-cf create-domain ssl-gateway-acceptance checker.ssltest4.com
-cf create-domain ssl-gateway-acceptance checker.ssltest2.com
-cf create-domain ssl-gateway-acceptance checker.ssltest.com
+cf create-domain ssl-gateway-acceptance misterX.com
+cf create-domain ssl-gateway-acceptance ssltest3.com
+cf create-domain ssl-gateway-acceptance ssltest4.com
+cf create-domain ssl-gateway-acceptance ssltest2.com
+cf create-domain ssl-gateway-acceptance ssltest.com
 
 bundle install
 rspec
